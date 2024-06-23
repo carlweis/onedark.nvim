f2b6ea1aada811c0edfc0ccf5d1d9a9efdd5c038
Carl Weis <carl@carlweis.com>
2024-06-23 19:31:13 -0400

    Address comment background color

--- lua/onedark/init.lua
+++ lua/onedark/init.lua
@@ -25,7 +25,7 @@ local colors = {
 local highlights = {
 
   -- Syntax Groups (descriptions and ordering from `:h w18`)
-  { hg = 'Comment', fg = colors.comment_grey, gui = 'italic', cterm = 'italic' }, -- any comment
+  { hg = 'Comment', fg = colors.comment_grey, bg = NONE,  gui = 'italic', cterm = 'italic' }, -- any comment
   { hg = 'Constant', fg = colors.cyan }, --any constant
   { hg = 'String', fg = colors.green }, --a string constant: "this is a string"
   { hg = 'Character', fg = colors.green }, --a character constant: 'c', '\n'
